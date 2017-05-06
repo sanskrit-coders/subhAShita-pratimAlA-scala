be42b2fe2b8625548029f0689badf1c25ce7e9d4
vishvAsaH <vishvas.vasuki@gmail.com>
2017-05-06 13:43:24 -0700

    asdf

--- README.md
+++ README.md
@@ -2,19 +2,19 @@
 * Initially started out with gradle project <https://github.com/sanskrit-coders/subhAShita-pratimAlA> and tried to import to scala, but failed.
 * Switched to this project from scratch, following tutorials here: <https://github.com/scala-android/sbt-android> .
 * Realized that one cannot import maven based modules (sanskritnlpjava and subhashita-db-sanskrit) in a sbt based project. [SO Question](http://stackoverflow.com/questions/38713822/intellij-idea-support-both-sbt-and-maven-on-a-single-project).
+* Further faced problems with sbt heap size when including big sanskritnlpjava jar.
 * Switched back to <https://github.com/sanskrit-coders/subhAShita-pratimAlA> .
-
-Current status: TODO: Stuff does not work after upgrade to using support-v13.
 
 # Initial setup
 * Link /home/vvasuki/.android/sbt/sdk to android sdk folder.
 * Just use the Build menu in Intellij to generate signed apk. If it fails, do packageRelease
 
 ## Quick sbt commands
-* You may need to drop the app/ below.
+* You may need to drop the `app/` below.
   * app/android:run
   * app/android:packageRelease
     * This uses credentials to keystore in local.properties, if available.
+    * APK will go in target/android/output/
 
 
 # Help links
--- src/main/scala/sanskritnlp/subhaashita_pratimaalaa/MainActivity.scala
+++ src/main/scala/sanskritnlp/subhaashita_pratimaalaa/MainActivity.scala
@@ -3,19 +3,21 @@ package sanskritnlp.subhaashita_pratimaalaa
 import android.graphics.drawable.Animatable
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import sanskritnlp.transliteration.transliterator
 
 class MainActivity extends AppCompatActivity {
-    // allows accessing `.value` on TR.resource.constants
-    implicit val context: MainActivity = this
+  // allows accessing `.value` on TR.resource.constants
+  implicit val context: MainActivity = this
 
-    override def onCreate(savedInstanceState: Bundle): Unit = {
-        super.onCreate(savedInstanceState)
-        // type ascription is required due to SCL-10491
-        val vh: TypedViewHolder.main = TypedViewHolder.setContentView(this, TR.layout.main)
-        vh.text.setText(s"Hello world, from ${TR.string.app_name.value}")
-        vh.image.getDrawable match {
-          case a: Animatable => a.start()
-          case _ => // not animatable
-        }
+  override def onCreate(savedInstanceState: Bundle): Unit = {
+    super.onCreate(savedInstanceState)
+    // type ascription is required due to SCL-10491
+    val vh = TypedViewHolder.setContentView(this, TR.layout.main).asInstanceOf[TypedViewHolder.main]
+    val quoteId = transliterator.transliterate("महा-सुभाषित-सङ्ग्रहे ", sourceScheme = "iast", destScheme = transliterator.scriptDevanAgarI)
+    vh.text.setText(s"$quoteId, from ${TR.string.app_name.value}")
+    vh.image.getDrawable match {
+      case a: Animatable => a.start()
+      case _ => // not animatable
     }
+  }
 }
